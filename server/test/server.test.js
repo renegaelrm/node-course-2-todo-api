a6c0ccd87f0a7dd23df539ca55a59fd29a5fe118
CTINTER\rene.rivera <reneriveract>
2017-04-07 11:55:12 -0700

    Add test cases for get todos by ID

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+        _id: new ObjectID,
         text: 'Texto nuevo 1'
     },{
+    _id: new ObjectID,
     text: 'Texto nuevo 2'
 }]
 
@@ -61,13 +64,34 @@ describe('POST /todos', () => {
     });
 
     describe('GET /todos', () => {
-        it('Debe obtener todos los registros', (done)=> {
+        it('Debe obtener todos los registros', (done) => {
             request(app)
             .get('/todos')
             .expect(200)
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+        });
+    });
+
+    describe('GET /todos/:id', () => {
+        it('Debería de regresar un documento', (done) => {
+            request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+        });
+
+        it('Regresa 404 si no lo encuentra', (done) => {
+            var hexId = new ObjectID().toHexString();
+
+            request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
             .end(done);
         });
     });
