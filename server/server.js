63351ac8a24a4aff9abc7081a5c485561bdfd065
CTINTER\rene.rivera <reneriveract>
2017-04-07 09:51:54 -0700

    Add test for GET /todos

--- server/server.js
+++ server/server.js
@@ -21,8 +21,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) =>{
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
 app.listen(3000, () => {
-    console.log('Iniciado en el puerto 3000');
+    console.log('Servidor sobre el puerto 3000');
 });
 
 module.exports = {app};
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+        text: 'Texto nuevo 1'
+    },{
+    text: 'Texto nuevo 2'
+}]
 
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => {
+        done();
+    });
 });
 
 describe('POST /todos', () => {
@@ -28,9 +37,10 @@ describe('POST /todos', () => {
             }
 
             Todo.find().then((todos) =>{
+                console.log(`Pruebas cantidad: ${todos.length}`);
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
-                donde();
+                done();
             }).catch((err) => done());
         });
     });
@@ -47,6 +57,18 @@ describe('POST /todos', () => {
             Todo.find().then((todos) => {
                 expect(todos.length).toBe(0);
             }).catch((err) => done(e));
+        });
+    });
+
+    describe('GET /todos', () => {
+        it('Debe obtener todos los registros', (done)=> {
+            request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
         });
     });
 });
