f67711c70db809caf7058d4d5c89e9a9880a0ae6
CTINTER\rene.rivera <reneriveract>
2017-04-10 09:40:29 -0700

    Bcrypt password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = 'abc123';
+
+/*bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('HASHING =====>', hash);
+    });
+});
+ */
+var hashedPassword = "$2a$10$X5Yd2f8gUzEEjQZ3s4RzBOkfsz9ZYxlHAPUlcMVj2nmKDm8uLM7Sq";
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log('RESULT =====> ',result);
+});
+
+
+/*var data = {
     id: 10
 };
 
@@ -11,7 +27,7 @@ console.log(token);
 var verify = jwt.verify(token, '123abc');
 console.log(verify);
 
-/*var message = 'Hola usuario 1';
+var message = 'Hola usuario 1';
 var hash = SHA256(message).toString();
 
 console.log(`Mensaje: ${message}`);
